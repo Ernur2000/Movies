d996fdefeca2e0f05f3a5ba3d58309e805499fe5
Ernurb <ernurb@bankffin.kz>
2024-11-11 17:25:11 +0500

    added chucker to for logging

--- app/src/main/java/com/example/moviesapp/di/AppModule.kt
+++ app/src/main/java/com/example/moviesapp/di/AppModule.kt
@@ -1,10 +1,16 @@
 package com.example.moviesapp.di
 
+import android.content.Context
+import androidx.compose.ui.platform.LocalContext
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.example.moviesapp.data.network.ApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,11 +22,27 @@ object AppModule {
     @Provides
     fun baseUrl() = "https://api.tvmaze.com/"
 
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY  // Logs request and response body
+    }
+
     @Provides
     @Singleton
-    fun providerRetrofit(baseUrl: String) : ApiService =
+    fun provideOkHttpClient(
+        @ApplicationContext context: Context // Inject Application Context here
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .addInterceptor(ChuckerInterceptor(context)) // Use the context here
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providerRetrofit(baseUrl: String, okHttpClient: OkHttpClient) : ApiService =
         Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
--- app/src/main/java/com/example/moviesapp/screens/SplashScreen.kt
+++ app/src/main/java/com/example/moviesapp/screens/SplashScreen.kt
@@ -1,5 +1,12 @@
 package com.example.moviesapp.screens
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
@@ -13,16 +20,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import com.example.moviesapp.MainViewModel
 import com.example.moviesapp.navigation.Screens
 import com.example.moviesapp.ui.theme.MoviesAppTheme
 import kotlinx.coroutines.delay
+import java.security.Permission
 
 @Composable
 fun SplashScreen(navController: NavController,viewModel: MainViewModel) {
+    val context = LocalContext.current
     var startAnimate by remember {
         mutableStateOf(false)
     }
@@ -30,11 +41,26 @@ fun SplashScreen(navController: NavController,viewModel: MainViewModel) {
         targetValue = if (startAnimate) 1f else 0f,
         animationSpec = tween(durationMillis = 3000)
     )
+
+    var launcherPermission: ManagedActivityResultLauncher<String, Boolean>? = null
+
+
+    launcherPermission = rememberLauncherForActivityResult(RequestPermission()) { areGranted ->
+
+    }
+
     LaunchedEffect(key1 = true) {
         startAnimate = true
         viewModel.getAllMovies()
         delay(4000)
         navController.navigate(Screens.Main.route)
+    }
+    LaunchedEffect(key1 = Unit) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                launcherPermission.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
     Splash(alpha = alphaAnimation.value)
 }
